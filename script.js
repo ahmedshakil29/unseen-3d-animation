fec8dce1f97f965e755a58a228b1b1f795a3b7c4
SHAKIL AHMED <ahmedshakil.aiub@gmail.com>
2024-04-30 23:05:11 +0600

    up

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 const showBtn = document.querySelector(".navBtn");
 const topNav = document.querySelector(".top-nav");
+imgCard = document.querySelectorAll(".img-card");
+imgCard.forEach((element) => {
+  element.addEventListener("click", function (e) {
+    //remove active class
+    imgCard.forEach((el) => {
+      el.classList.remove("active");
+    });
 
+    element.classList.add("active");
+  });
+});
 showBtn.addEventListener("click", function () {
   if (topNav.classList.contains("showNav")) {
     topNav.classList.remove("showNav");
