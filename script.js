870f162224068b6003fa2b6da7e944b975783705
SHAKIL AHMED <ahmedshakil.aiub@gmail.com>
2024-04-30 23:24:03 +0600

    up

--- script.js
+++ script.js
@@ -40,3 +40,23 @@ var swiper = new Swiper(".swiper", {
     clickable: true,
   },
 });
+
+// var swiper = new Swiper(".swiper", {
+//   effect: "coverflow",
+//   grabCursor: true,
+//   centeredSlides: true,
+//   slidesPerView: "auto",
+//   coverflowEffect: {
+//     rotate: 0,
+//     stretch: 0,
+//     depth: 100,
+//     modifier: 0,
+//     slideShadows: true,
+//   },
+//   spaceBetween: 60,
+//   // loop: true,
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+// });
