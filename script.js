2c433fb9394b611633e418ab2697e2561256dad6
SHAKIL AHMED <ahmedshakil.aiub@gmail.com>
2024-04-30 23:21:46 +0600

    up

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 const showBtn = document.querySelector(".navBtn");
 const topNav = document.querySelector(".top-nav");
+imgCard = document.querySelectorAll(".img-card");
+imgCard.forEach((element) => {
+  element.addEventListener("click", function (e) {
+    //remove active class
+    imgCard.forEach((el) => {
+      el.classList.remove("active");
+    });
 
+    element.classList.add("active");
+  });
+});
 showBtn.addEventListener("click", function () {
   if (topNav.classList.contains("showNav")) {
     topNav.classList.remove("showNav");
@@ -30,3 +40,23 @@ var swiper = new Swiper(".swiper", {
     clickable: true,
   },
 });
+
+// var swiper = new Swiper(".swiper", {
+//   effect: "coverflow",
+//   grabCursor: true,
+//   centeredSlides: true,
+//   slidesPerView: "auto",
+//   coverflowEffect: {
+//     rotate: 0,
+//     stretch: 0,
+//     depth: 100,
+//     modifier: 0,
+//     slideShadows: true,
+//   },
+//   spaceBetween: 60,
+//   // loop: true,
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+// });
